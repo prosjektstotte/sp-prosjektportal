143ae799bdfc735a8679171af6b417defcceed30
tarjeieo <ormestoyl@outlook.com>
2014-06-05 17:17:08 +0200

    Added v1 implementation of deploymanager

--- Glittertind.Sherpa/Glittertind.Sherpa.Library/Deploy/IDeployManager.cs
+++ Glittertind.Sherpa/Glittertind.Sherpa.Library/Deploy/IDeployManager.cs
@@ -8,5 +8,8 @@ namespace Glittertind.Sherpa.Library.Deploy
 {
     interface IDeployManager
     {
+        void UploadDesignPackage(string localFilePath, string siteRelativeUrlToLibrary);
+        void ActivateDesignPackage(Guid idOfPackage, string siteRelativeUrlToLibrary);
+        void ActivateDesignPackage(string nameOfPackage, string siteRelativeUrlToLibrary);
     }
 }
