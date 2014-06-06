22f437b513a3e9b3f30720e0e22833e7d3f69aa3
Ole Kristian Mørch-Storstein <olekms@puzzlepart.com>
2014-06-06 14:45:06 +0200

    adding FilePersistanceProvider

--- Glittertind.Sherpa/Glittertind.Sherpa.Library/Taxonomy/FilePersistanceProvider.cs
+++ Glittertind.Sherpa/Glittertind.Sherpa.Library/Taxonomy/FilePersistanceProvider.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Glittertind.Sherpa.Library.Taxonomy.Model;
 
 namespace Glittertind.Sherpa.Library.Taxonomy
 {
-    class FilePersistanceProvider
+    public class FilePersistanceProvider : IPersistanceProvider
     {
+        public string Path { get; set; }
+
+        public void Save(TermItemBase termSet)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var result = jsonSerializer.Serialize(termSet);
+            
+        }
+
+        public T Load<T>()
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var result = jsonSerializer.Deserialize<T>("");
+            return result;
+            
+        }
+        
     }
 }
