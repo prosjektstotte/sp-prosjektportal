8312453e940938373369f5625ee1227d08c35a19
Tarjei E. Ormestøyl <tarjei@outlook.com>
2014-06-19 13:55:54 +0200

    v1 of the new project form

--- Glittertind.SharePoint/Glittertind.SharePoint.Catalog/SPI/Modules/SiteAssets/javascript/gt.provisioning.js
+++ Glittertind.SharePoint/Glittertind.SharePoint.Catalog/SPI/Modules/SiteAssets/javascript/gt.provisioning.js
@@ -2,12 +2,46 @@
 GT.Provisioning = GT.Provisioning || {};
 
 GT.Provisioning.CreateWebFromCustomForm = function () {
-	console.log('Creating web from form yo');
-
 	event.preventDefault();
-    GT.Provisioning.CreateWeb();
+	var name = document.getElementById('projectNameInput').value;
+	var url = document.getElementById('projectUrlInput').value;
+	var desc = document.getElementById('projectDescriptionInput').value;
+
+	if (!name || !url) {
+		document.getElementById('projectValidation').innerHTML = "<div>Navn og URL er obligatoriske felter</div>";
+		return;
+	}
+	GT.Provisioning.CreateWeb(name, url, desc);
 };
 
-GT.Provisioning.CreateWeb = function(name, urlName, description, language, webtemplate) {
-    console.log('Creating web');
-};
+GT.Provisioning.CreateWeb = function (webTitle, webUrl, webDescription) {
+	// Just insert your reference to your feature and webtemplate here
+	var webTemplate = 'STS#0'; //'{4a110786-9683-4734-97ca-f2eca95ca377}#WTProject';
+	var webLanguage = 1044;
+
+	var clientContext = SP.ClientContext.get_current();
+	var currentWeb = clientContext.get_web();
+
+	var webCreateInfo = new SP.WebCreationInformation();
+	webCreateInfo.set_description(webDescription);
+	webCreateInfo.set_language(webLanguage);
+	webCreateInfo.set_title(webTitle);
+	webCreateInfo.set_url(webUrl);
+	webCreateInfo.set_useSamePermissionsAsParentSite(false);
+	webCreateInfo.set_webTemplate(webTemplate);
+
+	this.newWeb = currentWeb.get_webs().add(webCreateInfo);
+	clientContext.load(this.newWeb);
+	clientContext.executeQueryAsync(Function.createDelegate(this, function () {
+		var newUrl = this.newWeb.get_url()
+		console.log(newUrl);
+		//1. Stop long running operation
+		//2. Close modal dialog (if existing)
+		//3. Redirect to new site's '_layouts/15/permsetup.aspx?HideCancel=1'
+	}
+	), Function.createDelegate(this, function () {
+		console.log('fail');
+		//1. Stop long running operation
+		//2. Show error message
+	}));
+}
