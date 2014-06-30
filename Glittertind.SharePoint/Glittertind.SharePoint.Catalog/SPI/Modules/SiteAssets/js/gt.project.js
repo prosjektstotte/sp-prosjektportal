cfb948e985a6228e561f97233e27225ae19658e4
tarjeieo <ormestoyl@outlook.com>
2014-06-30 17:27:46 +0200

    Not done with creating content type with JSOM

---
+++ Glittertind.SharePoint/Glittertind.SharePoint.Catalog/SPI/Modules/SiteAssets/js/gt.project.js
@@ -0,0 +1,17 @@
+﻿var GT = GT || {};
+GT.Project = GT.Project || {};
+
+GT.Project.ChangePhase = function () {
+    alert('not implemented');
+};
+
+GT.Project.PopulateProjectPhasePart = function () {
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+        jQuery.when(GT.Common.GetPhaseFromCurrentItem()).then(function (phaseName) {
+            var phases = ['Konsept', 'Planlegge', 'Gjennomføre', 'Avslutte', 'Realisere'];
+            for (var ix = 0; ix < phases.length; ix++) {
+                jQuery('.projectPhases').append(GT.Common.GetPhaseLogoMarkup(phases[ix], phases[ix] == phaseName, true));
+            }
+        });
+    });
+};
