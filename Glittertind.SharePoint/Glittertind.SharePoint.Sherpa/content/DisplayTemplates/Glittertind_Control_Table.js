0179deb64fd9761bde892310c569578113d6b6e4
Tarjei E. Ormestøyl <tarjeieo@puzzlepart.com>
2017-02-17 09:21:45 +0100

    Minor rename fix

--- Glittertind.SharePoint/Glittertind.SharePoint.Sherpa/content/DisplayTemplates/Glittertind_Control_Table.js
+++ Glittertind.SharePoint/Glittertind.SharePoint.Sherpa/content/DisplayTemplates/Glittertind_Control_Table.js
@@ -145,7 +145,7 @@ ms_outHtml.push(''
 function RegisterTemplate_83e2c63a510448eeb5a9b6a183b01866() {
 
 if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
-  Srch.U.registerRenderTemplateByName("Control_List", DisplayTemplate_83e2c63a510448eeb5a9b6a183b01866);
+  Srch.U.registerRenderTemplateByName("Glittertind_Control_Table", DisplayTemplate_83e2c63a510448eeb5a9b6a183b01866);
 }
 
 if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
